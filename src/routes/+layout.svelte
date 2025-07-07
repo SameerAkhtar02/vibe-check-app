565bd8fa801f730d4d103426e220bcc66a63d203
Sameer Akhtar <sameer.akhtar.52.sa@gmail.com>
2025-07-07 19:45:23 +0530

    created +layout.js and added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Footer from '$lib/components/block/footer.svelte';
 	import '../app.css';
-	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+
 	let { children } = $props();
 </script>
 
